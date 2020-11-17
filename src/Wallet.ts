4175821c7f8756eaa31ba21b0f25c6b3b7ae3e8d
原罪 <531362022@qq.com>
2020-11-17 23:38:27 +0800

    feat: Generate all addresses

--- src/Wallet.ts
+++ src/Wallet.ts
@@ -1,17 +1,20 @@
 import { generateMnemonic, mnemonicToSeedSync, wordlists, validateMnemonic, mnemonicToEntropy } from 'bip39';
 import { bip32, payments } from 'bitcoinjs-lib';
-import { COIN, networkMap } from './libs/coin';
+import { COIN, COIN_META } from './libs/coin';
 
 export class Wallet {
   protected readonly seed: Buffer;
   protected readonly wordlist: string[];
   protected readonly mnemonic: string;
   protected readonly password?: string;
+  protected readonly purpose: number;
 
   constructor(strLength?: number, password?: string);
   constructor(mnemonic: string, password?: string);
   constructor(str?: number | string, password?: string) {
     this.wordlist = wordlists.english;
+    this.password = password;
+    this.purpose = 44;
 
     if (str === undefined) {
       str = 128;
@@ -27,7 +30,6 @@ export class Wallet {
     }
 
     this.seed = mnemonicToSeedSync(this.mnemonic);
-    this.password = password;
   }
 
   getEntropy(): string {
@@ -38,13 +40,15 @@ export class Wallet {
     return this.mnemonic;
   }
 
-  getAddress(coin: typeof COIN[number]) {
-    const child1 = this.getRoot(coin).derivePath("m/44'/0'/0'/0/0");
+  getAddress(coin: COIN, account: number = 0, index: number = 0, internal: boolean = false) {
+    const child = this
+      .getRoot(coin)
+      .derivePath(`m/${this.purpose}'/${COIN_META[coin].id}'/${account}'/${internal ? 1 : 0}/${index}`);
 
-    return payments.p2pkh({ pubkey: child1.publicKey }).address;
+    return payments.p2pkh({ pubkey: child.publicKey }).address;
   }
 
-  getRoot(coin: typeof COIN[number]) {
-    return bip32.fromSeed(this.seed, networkMap[coin]);
+  protected getRoot(coin: COIN) {
+    return bip32.fromSeed(this.seed, COIN_META[coin].network);
   }
 }
--- src/libs/coin.ts
+++ src/libs/coin.ts
@@ -1,10 +1,24 @@
 import { networks, Network } from 'bitcoinjs-lib';
 
-export const COIN = ['BTC', 'ETH'] as const;
+export type COIN = 'BTC' | 'ETH' | 'DOT';
 
-export const networkMap: {
-  [key in typeof COIN[number]]: Network;
+export const COIN_META: {
+  [key in COIN]: {
+    network: Network;
+    id: number;
+  };
 } = {
-  BTC: networks.bitcoin,
-  ETH: networks.bitcoin,
+  BTC: {
+    id: 0,
+    network: networks.bitcoin,
+  },
+  ETH: {
+    id: 60,
+    network: networks.bitcoin,
+  },
+  DOT: {
+    id: 354,
+    // FIXME:
+    network: networks.bitcoin,
+  },
 };
